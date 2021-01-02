13ecb7fcb0da98e295a1085e872490d090923ef9
mikobhoie2299 <ryan_miguel_sarabia@dlsu.edu.ph>
2021-01-03 01:56:51 +0800

    Add director's name query (2 table)

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="css/styles.css" rel="stylesheet">
-    
+
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     <title>IMDB DATASET</title>
 </head>
+
 <body class="p-5">
     <h1 class="text-center font-weight-bold">IMDB DATASET</h1>
     <div class="row p-4 d-flex justify-content-center align-items-center">
@@ -26,6 +26,7 @@
                 <option value="year">Year Shown</option>
                 <option value="ratinglessorequal">Rating Less than or Equal to</option>
                 <option value="ratinggreaterorequal">Rating Greater than or Equal to</option>
+                <option value="directorname">Director Name</option>
             </select>
         </div>
         <div class="mx-4 d-flex">
--- search.php
+++ search.php
@@ -5,33 +5,34 @@ $con = connection();
 
 $mode = $_REQUEST["mode"];
 $strSearch = $_REQUEST["strSearch"];
-$pageNum = $_REQUEST["pageNum"] -1;
+$pageNum = $_REQUEST["pageNum"] - 1;
 $pageLimit = 20;
 $offset = $pageNum * $pageLimit;
 
 switch ($mode) {
     case "name":
-        $sql = 'SELECT name FROM movies WHERE name LIKE "%'.$strSearch.'%" LIMIT '.$pageLimit.' OFFSET '.$offset;
+        $sql = 'SELECT name FROM movies WHERE name LIKE "%' . $strSearch . '%" LIMIT ' . $pageLimit . ' OFFSET ' . $offset;
         break;
     case "id":
-        $sql = 'SELECT name FROM movies WHERE movies.id = '.$strSearch.' LIMIT '.$pageLimit.' OFFSET '.$offset;
+        $sql = 'SELECT name FROM movies WHERE movies.id = ' . $strSearch . ' LIMIT ' . $pageLimit . ' OFFSET ' . $offset;
         break;
     case "year":
-        $sql = 'SELECT name FROM movies WHERE YEAR = '.$strSearch.' LIMIT '.$pageLimit.' OFFSET '.$offset;
+        $sql = 'SELECT name FROM movies WHERE YEAR = ' . $strSearch . ' LIMIT ' . $pageLimit . ' OFFSET ' . $offset;
         break;
     case "ratinglessorequal":
-        $sql = 'SELECT name FROM movies WHERE movies.rank <= '.$strSearch.' LIMIT '.$pageLimit.' OFFSET '.$offset;
+        $sql = 'SELECT name FROM movies WHERE movies.rank <= ' . $strSearch . ' LIMIT ' . $pageLimit . ' OFFSET ' . $offset;
         break;
     case "ratinggreaterorequal":
-        $sql = 'SELECT name FROM movies WHERE movies.rank >= '.$strSearch.' LIMIT '.$pageLimit.' OFFSET '.$offset;
+        $sql = 'SELECT name FROM movies WHERE movies.rank >= ' . $strSearch . ' LIMIT ' . $pageLimit . ' OFFSET ' . $offset;
+        break;
+    case "directorname":
+        $sql = 'SELECT name FROM movies JOIN movies_directors AS md ON movies.id = md.movie_id JOIN directors ON directors.id = md.director_id WHERE directors.last_name OR directors.first_name LIKE "%' . $strSearch . '%" LIMIT ' . $pageLimit . ' OFFSET ' . $offset;
         break;
     default:
         $sql = 'SELECT name FROM movies';
-  }
+}
 
-$result = $con->query($sql) or die ($con->connect_error);
-while($row = $result->fetch_assoc()){
-    echo "<li>".$row['name']."</li>";
+$result = $con->query($sql) or die($con->connect_error);
+while ($row = $result->fetch_assoc()) {
+    echo "<li>" . $row['name'] . "</li>";
 };
-
-?>
